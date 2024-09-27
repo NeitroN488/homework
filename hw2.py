bb24ad0d9a480952be0ee09c866ce005d17ee587
Ilya Bakulin <nl2211886@gmail.com>
2024-09-27 13:01:46 +0300

    homework

---
+++ hw2.py
@@ -0,0 +1,50 @@
+""" a = float(input())
+b = float(input())
+if (b!=0):
+    print(f'{a/b}')
+else:
+    print(f'ошибка') """
+
+"""a = int(input())
+if a>20:
+  print(f' Получилась скидка {round(a*35/100, 2)} и со скидкой {round(a-(a*35/100), 2)} ')
+else:
+  print(f' Скидки не будет')
+
+
+m = int(input("введите номер месяца: \n"))
+if m < 1 or m > 12:
+  print("ошибка")
+else:
+  if m > 2 and m <= 5:
+    print("весна")
+    if m == 3:
+      print(f'март')
+    elif m == 4:
+      print(f'апрель')
+    elif m == 5:
+      print(f'май')
+  elif m > 5 and m <= 8:
+    print("лето")
+    if m == 6:
+      print(f'июнь')
+    elif m == 7:
+      print(f'июль')
+    elif m == 8:
+      print(f'август')
+  elif m > 8 and m <= 11:
+    print("осень")
+    if m == 9:
+      print(f'сентябрь')
+    elif m == 10:
+      print(f'октябрь')
+    elif m == 11:
+      print(f'ноябрь')
+  elif m == 1 or m == 2 or m == 12:
+    print("зима")
+    if m == 1:
+      print(f'январь')
+    elif m == 2:
+      print(f'февраль')
+    elif m == 12:
+      print(f'декабрь') """
