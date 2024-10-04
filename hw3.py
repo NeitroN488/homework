d323df6f67e867d348b645926e946e6e12f4789e
Ilya Bakulin <nl2211886@gmail.com>
2024-10-04 12:14:52 +0300

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,4 +1,4 @@
-a = int(input("Введите верхний предел \n"))
+"""a = int(input("Введите верхний предел \n"))
 b = 0
 
 if a > 100 or a < 1: print("Ошибка")
@@ -7,7 +7,7 @@ else:
         b += i**3
     print(b) 
 
-""" for i in range(1, 10):
+for i in range(1, 10):
     for j in range(1,10):
         print('%3d' % (i*j),end='')
     print() 
@@ -15,5 +15,4 @@ else:
 for i in range(9,0, -1):
     for j in range(1,10):
         print('%3d' % (i*j),end='')
-    print()
- """
+    print()"""
