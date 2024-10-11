ab5a8496f420e8196dd9fbbafd1b20776141288b
Ilya Bakulin <nl2211886@gmail.com>
2024-10-11 12:48:57 +0300

    homework

---
+++ hw4.py
@@ -0,0 +1,15 @@
+""" a = input()
+leng = 0
+b = 1
+a = a.replace('h', '!')
+for i in range(len(a) - 1):
+    if a[i] == a[i + 1]:
+        b += 1
+        leng = max(leng, b)
+    else:
+        p = 1
+print(leng, st) """
+
+"""a = input()
+b = a[a.find('(') + 1 : a.find(')')]
+print(b)"""
