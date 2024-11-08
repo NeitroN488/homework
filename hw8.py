8b2791b3e26ea98882205e7524110026957cf76d
Ilya Bakulin <nl2211886@gmail.com>
2024-11-08 12:31:39 +0300

    Create hw8.py

---
+++ hw8.py
@@ -0,0 +1,10 @@
+import re
+
+''' st = input()
+m = re.findall(r'\b[0]?[0-9]?[10-23]?[:][0]?[0-9]?[10-59][:][0]?[0-9]?[10-59]', st)
+for i in range(len(m)):
+  st = st.replace(m[i], '(TBD)')
+n = re.findall(r'\b[0]?[0-9]?[10-23]?[:][0]?[0-9]?[10-59]', st)
+for i in range(len(n)):
+  st = st.replace(n[i], '(TBD)')
+print(st) '''
