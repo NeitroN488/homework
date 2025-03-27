c74b2213ba5ef07e3d8e15e81641317da622eb21
Ilya Bakulin <nl2211886@gmail.com>
2025-03-27 10:30:47 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,15 +1,27 @@
-""" a = input()
-leng = 0
-b = 1
-a = a.replace('h', '!')
-for i in range(len(a) - 1):
-    if a[i] == a[i + 1]:
-        b += 1
-        leng = max(leng, b)
-    else:
-        p = 1
-print(leng, st) """
+№1
+# class Car:
+#     def init(self, make, model):
+#         self.make = make
+#         self.model = model
 
-"""a = input()
-b = a[a.find('(') + 1 : a.find(')')]
-print(b)"""
+#     def getattr(self, item):
+#         return "This attribute is not available"
+# c = Car("Toyota", "Corolla")
+# print(c.make) 
+# print(c.color)
+# №2
+# class Rectangle:
+#     def init(self, width, height):
+
+#         self.width = width
+#         self.height = height
+
+#     def setattr(self, name, value):
+#         if name not in ('width', 'height'):
+#             raise AttributeError("Local attributes are not allowed")
+#         super().setattr(name, value)
+
+# r = Rectangle(10, 20)
+# r.width = 15
+# r.height = 25
+# r.color = 'red'
