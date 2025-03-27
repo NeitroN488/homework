0a395536c7808efa169bd85abc86cb58d2fd7603
Ilya Bakulin <nl2211886@gmail.com>
2025-03-27 10:29:51 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,50 +1,21 @@
-""" a = float(input())
-b = float(input())
-if (b!=0):
-    print(f'{a/b}')
-else:
-    print(f'ошибка') """
+class Recipe:
+#     def init(self, name, ingredients):
+#         self.name = name
+#         self.ingredients = ingredients
 
-"""a = int(input())
-if a>20:
-  print(f' Получилась скидка {round(a*35/100, 2)} и со скидкой {round(a-(a*35/100), 2)} ')
-else:
-  print(f' Скидки не будет')
+#     def print_ingredients(self):
+#         print(f"Ингредиенты для {self.name}:")
+#         for ingredient in self.ingredients:
+#             print(f"- {ingredient}")
 
+#     def cook(self):
+#         print(f"Сегодня мы готовим {self.name}.")
+#         print(f"Выполняем инструкцию по приготовлению блюда {self.name}...")
+#         print(f"Блюдо {self.name} готово!")
 
-m = int(input("введите номер месяца: \n"))
-if m < 1 or m > 12:
-  print("ошибка")
-else:
-  if m > 2 and m <= 5:
-    print("весна")
-    if m == 3:
-      print(f'март')
-    elif m == 4:
-      print(f'апрель')
-    elif m == 5:
-      print(f'май')
-  elif m > 5 and m <= 8:
-    print("лето")
-    if m == 6:
-      print(f'июнь')
-    elif m == 7:
-      print(f'июль')
-    elif m == 8:
-      print(f'август')
-  elif m > 8 and m <= 11:
-    print("осень")
-    if m == 9:
-      print(f'сентябрь')
-    elif m == 10:
-      print(f'октябрь')
-    elif m == 11:
-      print(f'ноябрь')
-  elif m == 1 or m == 2 or m == 12:
-    print("зима")
-    if m == 1:
-      print(f'январь')
-    elif m == 2:
-      print(f'февраль')
-    elif m == 12:
-      print(f'декабрь') """
+# spaghetti = Recipe("Спагетти болоньезе", ["Спагетти", "Фарш", "Томатный соус", "Лук", "Чеснок", "Соль"])
+# spaghetti.print_ingredients()
+# spaghetti.cook()
+# cake = Recipe("Кекс", ["Мука", "Яйца", "Молоко", "Сахар", "Сливочное масло", "Соль", "Ванилин"])
+# cake.print_ingredients()
+# cake.cook()
